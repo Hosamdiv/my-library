d2f83c550ff29446392b8c3ab00dd98a5553a83e
hosam rda <rdhosam712@gmail.com>
2025-02-13 14:57:10 +0200

    responsive

--- src/Layout/NavBar.tsx
+++ src/Layout/NavBar.tsx
@@ -43,12 +43,15 @@ const NavBar = () => {
           </div>
         </div>
         {/* NavBar */}
-        <div className="hidden md:block sticky top-0 bg-white text-black">
+        <div className=" hidden md:block sticky top-0 bg-white text-black">
           <nav className="flex items-center justify-between w-[90%] m-auto py-3 font-semibold">
             <h1 className="text-3xl font-bold">Logo</h1>
-            <ul className="flex space-x-7">
+            <ul className="flex space-x-8 text-[1.1rem] ">
               {navbar.map((e) => (
-                <li>
+                <li
+                  className="hover:text-blue-700 hover:-translate-y-1 transition delay-150
+                duration-300 ease-in-out "
+                >
                   <NavLink to="">{e.name}</NavLink>
                 </li>
               ))}
@@ -64,40 +67,49 @@ const NavBar = () => {
           </nav>
         </div>
         {/* Responsive NavBar */}
-        <div className="py-2 border-black">
+        <div className="py-2 border-black bg-white">
           <button
             onClick={() => setShowModal(true)}
-            className="ml-6 text-3xl text-black border-2 rounded md:hidden"
+            className="ml-6 text-3xl text-black border-2 rounded md:hidden
+            hover:text-red-700 transition delay-150 duration-300 ease-in-out
+            hover:-translate-y-1 hover:scale-110"
           >
             <IoMenu />
           </button>
-          {showModal && (
-            <div className="fixed inset-0  bg-[#282830e8] z-20">
-              <ul
-                className="w-[85%] mx-auto mt-[2rem]
-        rounded-2xl bg-[#18181b] px-[2rem] py-[1rem]"
-              >
-                <li className="text-right mb-7 flex justify-between">
-                  <h1 className="text-3xl font-bold">Logo</h1>
-                  <button
-                    className=" text-3xl"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <FaRegWindowClose />
-                  </button>
+
+          <div
+            className={`fixed inset-0 bg-[#282830e8] z-20 flex justify-center items-center 
+        transition-all duration-500 ease-in-out 
+        ${
+          showModal
+            ? "opacity-100 scale-100"
+            : "opacity-0 scale-95 pointer-events-none"
+        }`}
+          >
+            <ul className="w-[85%] mx-auto mt-[2rem] rounded-2xl bg-[#18181b] px-[2rem] py-[1rem] transition-all duration-500 ease-in-out">
+              <li className="text-right mb-7 flex justify-between">
+                <h1 className="text-3xl font-bold">Logo</h1>
+                <button
+                  className="text-3xl hover:text-red-700 transition duration-300 ease-in-out
+              hover:-translate-y-1 hover:scale-110"
+                  onClick={() => setShowModal(false)}
+                >
+                  <FaRegWindowClose />
+                </button>
+              </li>
+
+              {navbar.map((e, idx) => (
+                <li
+                  key={idx}
+                  className={`pb-4 pt-2 transition-all duration-300 ease-in-out
+              ${idx === navbar.length - 1 ? "" : "border-b border-gray-600"}
+              hover:text-cyan-500 hover:-translate-y-1`}
+                >
+                  <Link to="">{e.name}</Link>
                 </li>
-                {navbar.map((e) => (
-                  <li
-                    className={`border-b-none pb-4 pt-2 
-                    hover:text-cyan-500 hover:text-xl transition-all duration-300 ease-in-out
-                  `}
-                  >
-                    <Link to="">{e.name}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </>
@@ -109,6 +121,6 @@ const navbar = [
   { name: "Home", link: "" },
   { name: "Magazines", link: "" },
   { name: "Books", link: "" },
-  { name: "TextBooks", link: "" },
+  { name: "Text Books", link: "" },
   { name: "Product", link: "" },
 ];
