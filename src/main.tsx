ca52be80d90f6f0dba4c6db44c1d7311d9aefa0e
hosam rda <rdhosam712@gmail.com>
2025-02-13 15:02:19 +0200

    resposive

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Provider as ProviderRedux } from "react-redux";
-import { store } from "./app/store.ts";
+// import { Provider as ProviderRedux } from "react-redux";
+// import { store } from "./app/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <ProviderRedux store={store}>
+    {/* <ProviderRedux store={store}> */}
       <App />
-    </ProviderRedux>
+    {/* </ProviderRedux> */}
   </QueryClientProvider>
 );
